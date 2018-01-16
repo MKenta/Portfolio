22a61a89d88c9e3272555e81ff3f26e219fdeebe
miyachik <atto.mark56@gmail.com>
2018-01-16 11:47:03 +0900

    Profile用に

---
+++ components/Social.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <div class="fa-4x">
+      <a href="https://twitter.com/_miyachik" target="_blank"> <i class="fa fa-twitter-square icon"></i> </a>
+      <a href="https://github.com/miyachik" target="_blank"> <i class="fa fa-github-square icon"></i> </a>
+      <a href="https://www.facebook.com/kenta.miyachi.98" target="_blank"> <i class="fa fa-facebook-square icon"></i> </a>
+    </div>
+  </div>
+</template>
+
+<style>
+
+.icon {
+   padding: 20px;
+}
+
+</style>
