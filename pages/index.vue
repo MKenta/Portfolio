aaa243aaec180e9e4e0a08943255b4e111df9e57
miyachik <atto.mark56@gmail.com>
2018-01-29 18:45:05 +0900

    LivePhotos一旦止める

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,6 @@
       <h2 class="subtitle">
         Ohter
       </h2>
-      <livePhoto/>
     </div>
   </section>
 </template>
@@ -20,28 +19,11 @@
 <script>
 import Logo from '~/components/Logo.vue'
 import Social from '~/components/Social.vue'
-import LivePhoto from '~/components/LivePhoto.vue'
 
 export default {
   components: {
     Logo,
-    Social,
-    LivePhoto
-  },
-  mounted () {
-    if (process.browser) {
-      var LivePhotosKit = require('livephotoskit')
-      var player = new LivePhotosKit.Player(document.getElementsByClassName('live-photo')[0])
-      player.photoSrc = 'https://s3-ap-northeast-1.amazonaws.com/liviewm-dev/articles/images/' + 'shabon_song.mov-00m40s09-A05BC584-D6E0-41BF-BB2C-DF9BA8922E4E' + '.JPG'
-      player.videoSrc = 'https://s3-ap-northeast-1.amazonaws.com/liviewm-dev/articles/movies/' + 'shabon_song.mov-00m40s09-A05BC584-D6E0-41BF-BB2C-DF9BA8922E4E' + '.MOV'
-      player.videoHeight = 750 / 2 + 'px'
-      player.photoHeight = 750 / 2 + 'px'
-      player.videoWidth = 1334 / 2 + 'px'
-      player.photoWidth = 1334 / 2 + 'px'
-      player.style.height = 750 / 2 + 'px'
-      player.style.width = 1334 / 2 + 'px'
-      console.log('livephotoskit is loaded.')
-    }
+    Social
   }
 }
 </script>
